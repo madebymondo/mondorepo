32e68f06b7ac85aeeabfb2c4cfb83f7b012b3d28
mondodavid <david@madebymondo.com>
2023-07-15 00:34:09 -1000

    feat: add Mondo logic with runDev method

---
+++ packages/mondo/src/lib/mondo.ts
@@ -0,0 +1,27 @@
+import { getSiteInternals } from '@/utils/internals.js';
+import { runDevServer } from '@/dev/server.js';
+import { ConfigOptions, DynamicallyImportedFile } from '@mondo/mondo';
+
+interface MondoConstructor {
+	/** Dynamically imported mondo.config.ts file */
+	configData: DynamicallyImportedFile;
+}
+
+export class Mondo {
+	siteInternals: ConfigOptions;
+
+	constructor(options: MondoConstructor) {
+		/** Parse config file data */
+		this.siteInternals = getSiteInternals(options.configData);
+	}
+
+	/**
+	 * Command run with mondo dev. Initializes the
+	 * dev server and environment.
+	 */
+	async runDev() {
+		await runDevServer({
+			internals: this.siteInternals,
+		});
+	}
+}
