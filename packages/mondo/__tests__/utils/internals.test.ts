3bed550a3cd6a872cf762784bb2529fdc6dd6add
mondodavid <david@madebymondo.com>
2023-07-19 01:58:24 -1000

    style: replace 'test' with 'it'

--- packages/mondo/__tests__/utils/internals.test.ts
+++ packages/mondo/__tests__/utils/internals.test.ts
@@ -1,32 +1,31 @@
-import { describe, expect, test } from '@jest/globals';
-import { ConfigOptions, DynamicallyImportedFile } from '@mondo/mondo';
+import { describe, expect, it } from '@jest/globals';
+import { ConfigOptions, DefaultDynamicallyImportedFile } from '@mondo/mondo';
 import {
 	getSiteInternals,
 	DEFAULT_MONDO_CONFIGURATION,
 } from '@/utils/internals.js';
 
-const MOCK_CONFIGURATION_DATA: DynamicallyImportedFile = [
-	{
-		key: 'default',
-		callback: {
-			root: 'project-root',
+const MOCK_CONFIGURATION_DATA: DefaultDynamicallyImportedFile = {
+	default: {
+		root: 'project-root',
+		server: {
 			port: 8080,
-		} as ConfigOptions,
-	},
-];
+		},
+	} as ConfigOptions,
+};
 
 describe('Configuration should parsed correctly', () => {
-	test('Return the default configuration if no configuration is exported', () => {
+	it('Return the default configuration if no configuration is exported', () => {
 		const internals = getSiteInternals();
 		expect(internals).toBe(DEFAULT_MONDO_CONFIGURATION);
 	});
 
-	test(`Configuration should update the 'port' if it's set in the config`, () => {
+	it(`Configuration should update the 'port' if it's set in the config`, () => {
 		const internals = getSiteInternals(MOCK_CONFIGURATION_DATA);
-		expect(internals.port).toBe(8080);
+		expect(internals.server.port).toBe(8080);
 	});
 
-	test(`Configuration should update the 'root' if it's set in the config`, () => {
+	it(`Configuration should update the 'root' if it's set in the config`, () => {
 		const internals = getSiteInternals(MOCK_CONFIGURATION_DATA);
 		expect(internals.root).toBe('project-root');
 	});
