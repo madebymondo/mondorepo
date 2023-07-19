2982c18198bf85a499ccca5c40783f2b39734fd2
mondodavid <david@madebymondo.com>
2023-07-19 00:45:21 -1000

    feat: add ServerOptions

--- packages/mondo/src/types/mondo.d.ts
+++ packages/mondo/src/types/mondo.d.ts
@@ -1,8 +1,25 @@
 declare module '@mondo/mondo' {
 	/** Options that can be passed to the mondo.config.ts file */
 	interface ConfigOptions {
-		/** Project root directory path */
+		/** Project root directory path. Defaults to "src" */
 		root?: string;
+		/** Directory where routes are stored. Defaults to "src/pages" (relative to root) */
+		pagesDirectory?: string;
+		/** Directory where template-related files are stored. Defaults to "src/views" (relative to root) */
+		viewsDirectory?: string;
+		/** Directory where global data files passed to all routes are store. Defaults to "src/data" (relative to root) */
+		globalDataDirectory?: string;
+		server: ServerOptions;
+	}
+
+	/** Configuration options passed to the server */
+	interface ServerOptions {
+		/** Template engine to use */
+		templateEngine: 'njk';
+		/** Directory to serve static files and assets from. Defaults to "public" */
+		staticFilesPath?: string;
+		/** Route that serves the static files. Defaults to "/public" */
+		staticFilesRoute?: string;
 		/** Port for development and production server mode */
 		port?: number;
 	}
