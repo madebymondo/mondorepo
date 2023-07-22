e160469b4221fbd9984a205490da3b39eca03c9e
mondodavid <david@madebymondo.com>
2023-07-21 21:27:42 -1000

    feat: add mock pages data

---
+++ demos/site1/src/pages/[page].ts
@@ -0,0 +1,31 @@
+const pages = [
+	{
+		slug: 'page-one',
+		title: 'Page One',
+	},
+	{
+		slug: 'pages/page-two',
+		title: 'Page Two',
+	},
+];
+
+export async function createPage(ctx) {
+	// Matches the [page].ts
+	const slug = ctx.params.page;
+
+	const pageData = pages.find((page) => page.slug === slug);
+
+	return {
+		template: 'base.njk',
+		title: pageData?.title,
+		slug: pageData?.slug,
+	};
+}
+
+// This function is only needed for SSG
+export async function createPaths() {
+	return pages.map((pageData) => ({
+		// Return the [page] param for SSG
+		page: pageData.slug,
+	}));
+}
