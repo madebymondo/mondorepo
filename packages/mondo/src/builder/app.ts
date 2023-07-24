7dfb3de835233e28f84f3df54650e49672764047
mondodavid <david@madebymondo.com>
2023-07-24 12:16:23 -1000

    feat: integrate global data in prod server

--- packages/mondo/src/builder/app.ts
+++ packages/mondo/src/builder/app.ts
@@ -1,5 +1,6 @@
 /** Production server file */
 import express, { Express, Request, Response, NextFunction } from 'express';
+import fs from 'fs';
 import path from 'path';
 //@ts-ignore
 import CONFIG_DATA from './config.json' assert { type: 'json' };
@@ -17,17 +18,45 @@ const app: Express = express();
  */
 const mergedRoutes = generateMergedRoutes(path.join(buildDirectory, 'pages'));
 
-app.use('/public', express.static(path.join(buildDirectory, 'public')));
+/** Handle global data middleware  */
+const globalDataDirectory = path.join(buildDirectory, 'data');
+const globalDataDirectoryExists = fs.existsSync(globalDataDirectory);
 
-app.use('/', express.static(path.join(buildDirectory, 'html')));
+if (globalDataDirectoryExists) {
+	const globalDataFiles = fs.readdirSync(globalDataDirectory);
 
-//TODO: Implement global data
+	(async function () {
+		for await (const dataFile of globalDataFiles) {
+			/* Read each data file and get it's default exported function */
+			const dataFileFunctions = await import(
+				path.join(globalDataDirectory, dataFile)
+			);
+			const dataFileContents = dataFileFunctions.default;
+
+			/* Get the name of the file and the return value
+			of the callback */
+			const dataBasename = dataFile
+				.replace(globalDataDirectory, '')
+				.replace('.js', '');
+
+			const data = await dataFileContents();
+
+			/* Set the value of each data file to a local global
+			 * variable in the app */
+			app.locals[dataBasename] = data;
+		}
+	})();
+}
 
 const engine = new TemplateEngine({
 	engine: templateEngine,
 	viewsDirectory,
 	app,
 });
+
+app.use('/public', express.static(path.join(buildDirectory, 'public')));
+
+app.use('/', express.static(path.join(buildDirectory, 'html')));
 
 for (const routeFile of mergedRoutes) {
 	const route = await import(routeFile);
