4fe930c509cf9e9a82d4a97eda42dcb80d3ca525
mondodavid <david@madebymondo.com>
2023-07-24 01:09:11 -1000

    feat: export utils for build server

--- packages/mondo/src/core.ts
+++ packages/mondo/src/core.ts
@@ -1,7 +1,13 @@
+import { TemplateEngine } from '@/utils/templates.js';
+import { generateMergedRoutes } from '@/utils/router.js';
 import { logGreen, logYellow, logRed } from '@/utils/logger.js';
+import { mergeDeep } from '@/utils/helpers.js';
 import { CreatePageContext, CreatePage, ConfigOptions } from '@mondo/mondo';
 
 export {
+	generateMergedRoutes,
+	TemplateEngine,
+	mergeDeep,
 	logGreen,
 	logYellow,
 	logRed,
