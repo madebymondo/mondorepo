dc1b9bf2b7bd100fba00a138a10382db3fc17f13
mondodavid <david@madebymondo.com>
2023-08-07 00:49:19 -1000

    feat: add filter demos

--- examples/simple-server/mondo.config.js
+++ examples/simple-server/mondo.config.js
@@ -1,8 +1,25 @@
 export default {
 	renderMode: 'server',
 	server: {
-		port: 8000,
+		port: 3000,
+	},
+	templateFilters: {
+		filterExtension: addFilterExtensionFilter,
+		logEnv: logEnvFilter,
+		asyncLogEnv: testAsyncFilter,
 	},
 	// Directories to passthrough to build relative to root
 	passthrough: ['cms'],
 };
+
+function addFilterExtensionFilter(value) {
+	return `${value} was passed to the filter`;
+}
+
+function logEnvFilter(env) {
+	console.log('Env Filter', env);
+}
+
+async function testAsyncFilter(env) {
+	console.log('Async env filter');
+}
