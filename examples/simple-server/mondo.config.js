87baf609c8c81dc397a1f2ca03fd489130551530
mondodavid <david@madebymondo.com>
2023-08-08 13:55:06 -1000

    feat: add serverHook example

--- examples/simple-server/mondo.config.js
+++ examples/simple-server/mondo.config.js
@@ -2,6 +2,13 @@ export default {
 	renderMode: 'server',
 	server: {
 		port: 3000,
+		serverHook: (app, engine) => {
+			app.use('*', (req, res, next) => {
+				console.log('This is a global server midleware');
+				console.log(engine._renderTemplate);
+				next();
+			});
+		},
 	},
 	templateFilters: {
 		filterExtension: addFilterExtensionFilter,
